1d611e43d056d1ad908b4daf2eb22abd2d4891de
denis-scher <denis11192@gmail.com>
2024-02-11 16:46:01 +0200

    added a function that checks the available directions the player can go after a move.

--- solver.py
+++ solver.py
@@ -1,10 +1,10 @@
 
 
-grid = [[0, 0, 0, 1, 0],
+grid = [[0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0]]
+        [0, 0, 0, 0, 0]]
 
 
 class Player:
@@ -37,6 +37,26 @@ class Player:
                     break
             self.x = self.x - x_add
 
+        if direction == "down":
+            y_add = 0
+            for i in range(self.y + 1, 5):
+                if grid[i][self.x] == 0:
+                    grid[i][self.x] = 1
+                    y_add = y_add + 1
+                else:
+                    break
+            self.y = self.y + y_add
+
+        if direction == "up":
+            y_add = 0
+            for i in range(self.y-1, -1, -1):
+                if grid[i][self.x] == 0:
+                    grid[i][self.x] = 1
+                    y_add = y_add + 1
+                else:
+                    break
+            self.y = self.y - y_add
+
     def setLocation(self, y, x):
         if grid[y][x] == 1:
             print("The location is invalid")
@@ -44,12 +64,42 @@ class Player:
         self.y = y
         grid[y][x] = 1
 
-#    def checkAvailability:
+    def checkOptions(self):
+        up = True
+        down = True
+        left = True
+        right = True
+        _list = []
+        if self.x == 4:
+            right = False
+        elif grid[self.y][self.x + 1] == 1:
+            right = False
+        if self.x == 0:
+            left = False
+        elif grid[self.y][self.x - 1] == 1:
+            left = False
+        if self.y == 4:
+            down = False
+        elif grid[self.y + 1][self.x] == 1:
+            down = False
+        if self.y == 0:
+            up = False
+        elif grid[self.y - 1][self.x] == 1:
+            up = False
+        if up is True:
+            _list.append("up")
+        if down is True:
+            _list.append("down")
+        if left is True:
+            _list.append("left")
+        if right is True:
+            _list.append("right")
+        return _list
 
 
-
-player = Player(4,4)
-player.move("left")
+player = Player(0,0)
+player.move("right")
 for i in grid:
     print(i)
 print(player.getLocation())
+print(player.checkOptions())
